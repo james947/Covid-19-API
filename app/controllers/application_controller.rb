c40494776b399c8901fd1452586f93c35dc13095
james947 <jmuriuki947@gmail.com>
2020-03-22 00:28:35 +0300

    update api endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,23 +6,27 @@ class ApplicationController < Sinatra::Base
         content_type :json 
     end
 
-    get '/' do
+    get '/api/v1/all_cases' do
         all_cases = AllCases.where('latest IS true')
         { data: all_cases }.to_json
     end
 
-    get '/active_cases' do
+    get '/api/v1/active_cases' do
         active_cases = ActiveCases.where('latest IS true')
         { data: active_cases }.to_json
     end
 
-    get '/closed_cases' do
+    get '/api/v1/closed_cases' do
         closed_cases = ClosedCases.where('latest IS true')
         { data: closed_cases }.to_json
     end
 
-    get '/countries_data' do        
-         countries_data = CountriesData.where('latest IS true')
+    get '/api/v1/countries_data' do  
+        if params[:country]
+            countries_data = CountriesData.country(params[:country].capitalize).where('latest IS true')
+        else
+            countries_data=CountriesData.where('latest IS true')
+        end
         { data: countries_data }.to_json
     end  
 end
--- app/models/countries_data.rb
+++ app/models/countries_data.rb
@@ -1,2 +1,3 @@
 class CountriesData < ActiveRecord::Base
+    scope :country, ->(country) { where(country_or_other: country)}
 end

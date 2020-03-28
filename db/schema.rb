76565c27b84386515c224cb7d0c085d75863bb99
james947 <jmuriuki947@gmail.com>
2020-03-28 19:02:18 +0300

    Add first case to column to countries data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_095251) do
+ActiveRecord::Schema.define(version: 2020_03_28_133552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_095251) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "latest", default: false
     t.integer "total_deaths_1m_pop"
-    t.integer "first_case"
+    t.string "first_case"
   end
 
 end
